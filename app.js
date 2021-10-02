c7ddd2f3f8ac3c8d10b9e6d60a7cbe5cb9e34fff
David Santana <89168806+Davidbiririto@users.noreply.github.com>
2021-10-01 23:05:07 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+function Chutar(){
+    
+
+
+//sortear um numero aleatorio secreto
+let numeroSecreto = parseInt(Math.random()*11)
+console.log(numeroSecreto)
+
+
+//pegar um numero digitado
+let chute = parseInt(document.querySelector("#valor").value)
+console.log(numeroSecreto , chute)
+
+/**
+
+     * OPERADORES RELACIONAIS
+
+     * < --> menor
+
+     * > --> maior
+
+     * <= --> menor ou igual
+
+     * >= --> maior ou igual
+
+     * == --> igual
+
+     * != --> diferente
+
+     */
+
+//comparar o numero digitado com o numero aleatorio secreto
+if(numeroSecreto == chute){
+
+
+//se tiver correto e os numeros forem iguais, mostrar a mensagem que a pessoa ganhou
+document.querySelector(".resultado").innerHTML = "Você acertou"
+
+}
+else
+{
+
+//se tiver errado e os numeros forem diferentes, mostrara mensagem que a pessoa errou
+document.querySelector(".resultado").innerHTML = "Voçê errou"
+
+
+}
+
+
+
+}
